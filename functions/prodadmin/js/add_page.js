6e8842e7e49868466abbc42036cd5b70d02cf56c
saitharunkumar10 <saitharunkumar10>
2020-02-03 18:39:40 -0600

    image

--- functions/prodadmin/js/add_page.js
+++ functions/prodadmin/js/add_page.js
@@ -1,3 +1,30 @@
 function add_page(){
+    add_page_secured()
+}
+
+function add_page_secured(){
     glPageContent.innerHTML = '<h1>Add Page</h1>'
+    glPageContent.innerHTML =`
+    <a href='/add' class="btn btn-outline-primary">Home</a>
+    <a href='/show' class="btn btn-outline-primary">Show Product</a>
+    <div class ="form-group">
+        Name: <input class="form-group" type="text" id="name" />
+    </div>
+    <div class="form-group">
+    Summary:<br>
+    <textarea class="form-control" id="summary" cols="40" rows="5"></textarea>
+    </div>
+    <div class ="form-group">
+        Price: <input class="form-group" type="text" id="price" />
+    </div>
+    <div class ="form-group">
+        Image: <input type="file" id="imageButton" value="upload" />
+    </div>
+    <button class="btn btn-primary" type="button" onclick="addProduct()">ADD</button>
+    `;
+
+    const imageButton = document.getElementById('imagebutton')
+    imageButton.addEventListener('change', e =>{
+        console.log('file upload', e.target.files[0])
+    })
 }
--- functions/prodadmin/js/home_page.js
+++ functions/prodadmin/js/home_page.js
@@ -1,3 +1,22 @@
+
 function home_page(){
-    glPageContent.innerHTML = '<h1>home Page</h1>'
+    home_page_secured()
+}
+
+function home_page_secured(){
+    glPageContent.innerHTML ='<h1>Home Page</h1>'
+    glPageContent.innerHTML +=`
+    <a href='/add' class="btn btn-outline-primary">Add A Product</a>
+    <a href='/show' class="btn btn-outline-primary">Show Product</a>
+    <button class ="btn btn-outline-danger" type="button" onclick="logOut()">Log Out</button>
+    `;
+}
+
+async function logOut(){
+    try {
+        await firebase.auth().signOut()
+         window.location.href = '/login'
+    } catch (e) {
+        window.location.href = '/login'
+    }
 }
